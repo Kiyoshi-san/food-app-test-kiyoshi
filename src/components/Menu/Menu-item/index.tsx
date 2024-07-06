816f4d3302bbdcd49bca574363b88745fbeab828
Kiyoshi <kiyoshibudokan@hotmail.com>
2024-07-06 10:52:19 -0300

    Made the nav menu responsive

--- src/components/Menu/Menu-item/index.tsx
+++ src/components/Menu/Menu-item/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import style from "./menu-item.module.css";
 import type { TMenuItem } from "model/menu";
 import { Link } from "react-router-dom";
@@ -12,6 +12,15 @@ type TMenuItems = {
 
 const MenuItem = ({ menuItems }: TMenuItems) => {
   const dispatch = useDispatch();
+
+  const [selectedItem, setSelectedItem] = useState<string>();
+
+  useEffect(() => {
+    const currentItem = menuItems.find((item) => item.key === page);
+    if (currentItem) {
+      setSelectedItem(currentItem.text);
+    }
+  }, [menuItems]);
 
   const page = useGetPage();
   const { navMenuActive } = useSelector(
@@ -29,20 +38,23 @@ const MenuItem = ({ menuItems }: TMenuItems) => {
           className={`${style.hamburger} ${navMenuActive ? style.active : ""}`}
         ></div>
       </div>
-      <nav>
-        <ul>
-          {menuItems.map((item) => (
-            <li key={item.key}>
-              <Link
-                to={`/${item.key}`}
+      <div className={style.titleContainer}>
+        <h1>{selectedItem}</h1>
+      </div>
+      <div className={style.menuItemsContainer}>
+        <nav>
+          <ul>
+            {menuItems.map((item) => (
+              <li
+                key={item.key}
                 className={page === item.key ? style.active : ""}
               >
-                {item.text}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
+                <Link to={`/${item.key}`}>{item.text}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 };
